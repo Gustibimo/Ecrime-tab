75d819d02407dfc4c6bb9edb7122663e5a7f2ccc
Gusti Bimo <gstbimo@gmail.com>
2015-06-03 11:58:41 +0700

    Ini

---
+++ app/src/main/java/samples/exoguru/materialtabs/Tab1.java
@@ -0,0 +1,41 @@
+package samples.exoguru.materialtabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Edwin on 15/02/2015.
+ */
+public class Tab1 extends Fragment {
+WebView webView;
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // get the url to open
+        // set up the WebView
+        webView = (WebView) getView().findViewById(R.id.webView);
+        webView.setWebViewClient(new MyBrowser());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl("http://www.google.com");
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_1,container,false);
+        return v;
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
